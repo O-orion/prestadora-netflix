4c585123e1efde44ef49a49fa086d4832c088b6e
Luca Viana <78035167+O-orion@users.noreply.github.com>
2023-10-02 15:01:50 -0300

    page register

--- src/app/pages/registrar-page/registrar-page.component.ts
+++ src/app/pages/registrar-page/registrar-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registrar-page',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./registrar-page.component.scss']
 })
 export class RegistrarPageComponent {
+  hide: boolean = true;
+  formRegister!: UntypedFormGroup;
+
+  constructor(private formBuilder: UntypedFormBuilder) {}
+
+  ngOnInit() {
+    this.formRegister = this.formBuilder.group({
+      nome: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  public registrarUsuario(): void {
+    
+  }
 
 }
